ff18edaf55151900379b3251ad72e19fa323d313
diegocrocha <diego.rocha@bandtec.com.br>
2021-05-18 21:49:58 -0300

    post renda

---
+++ src/main/java/com/economigos/economigostelegram/form/RendaForm.java
@@ -0,0 +1,72 @@
+package com.economigos.economigostelegram.form;
+
+
+import javax.validation.constraints.NotNull;
+
+public class RendaForm {
+
+    private Long idConta;
+    private Long idCategoria;
+    private Double valor;
+    private Boolean recebido;
+    private String descricao;
+    private Boolean fixo;
+    private String dataPagamento;
+
+    public Long getIdConta() {
+        return idConta;
+    }
+
+    public void setIdConta(Long idConta) {
+        this.idConta = idConta;
+    }
+
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Boolean getRecebido() {
+        return recebido;
+    }
+
+    public void setRecebido(Boolean recebido) {
+        this.recebido = recebido;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getFixo() {
+        return fixo;
+    }
+
+    public void setFixo(Boolean fixo) {
+        this.fixo = fixo;
+    }
+
+    public String getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(String dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+}

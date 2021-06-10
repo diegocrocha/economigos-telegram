2f019248ed005fa76793cc06b282687c109b9483
Vitor Markz <61842891+VMarkz@users.noreply.github.com>
2021-06-10 09:41:48 -0300

    Update ServerConfig.java

--- src/main/java/com/economigos/economigostelegram/utils/ServerConfig.java
+++ src/main/java/com/economigos/economigostelegram/utils/ServerConfig.java
@@ -7,8 +7,8 @@ public class ServerConfig {
         if (System.getenv("SERVICE_URL") != null){
             baseUrl = System.getenv("SERVICE_URL");
         }else{
-            //baseUrl = "http://ec2-34-236-53-23.compute-1.amazonaws.com:8080/";
-            baseUrl = "http://localhost:8080/";
+            baseUrl = "http://ec2-34-236-53-23.compute-1.amazonaws.com:8080/";
+            //baseUrl = "http://localhost:8080/";
         }
 
         return baseUrl;
